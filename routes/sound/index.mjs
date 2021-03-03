4f4de68f1a57fb77f12cbaf7dae073406e777141
neolectron <neolectron@codinglab.io>
2021-03-03 20:56:26 +0100

    feat(soundRoute): returns youtubeID without .flac extension

--- routes/sound/index.mjs
+++ routes/sound/index.mjs
@@ -1,50 +1,50 @@
 import { createWriteStream } from 'fs';
 import { readdir, rm } from 'fs/promises';
 import FileType from 'file-type';
-import { join } from 'path';
+import { join, basename, extname } from 'path';
 
 const soundRoute = async (fastify, options) => {
   const { UnsupportedMediaType, NotFound } = fastify.httpErr;
   const { DATA_FOLDER } = process.env;
 
-  // get host with req.get('host') to send absolute web urls.
-
+  // PUBLIC ROUTES
   fastify.get('/', async (req, reply) => {
-    return readdir(DATA_FOLDER);
+    const files = await readdir(DATA_FOLDER);
+    return files.map((file) => basename(file, extname(file)));
   });
 
-  fastify.post('/', async (req, reply) => {
-    const { file, fields } = await req.file();
-    const fileStream = await FileType.stream(file);
+    fastify.post('/', async (req, reply) => {
+      const { file, fields } = await req.file();
+      const fileStream = await FileType.stream(file);
 
-    if (
-      fileStream.fileType?.ext !== 'flac' ||
-      fileStream.fileType?.mime !== 'audio/x-flac'
-    ) {
+      if (
+        fileStream.fileType?.ext !== 'flac' ||
+        fileStream.fileType?.mime !== 'audio/x-flac'
+      ) {
       return UnsupportedMediaType();
-    }
+      }
 
-    const url = new URL(fields.youtubeInput.value);
-    const name = url.searchParams.get('v');
-    const destination = join(DATA_FOLDER, `${name}.flac`);
+      const url = new URL(fields.youtubeInput.value);
+      const name = url.searchParams.get('v');
+      const destination = join(DATA_FOLDER, `${name}.flac`);
 
-    const writeStream = createWriteStream(destination);
-    fileStream.pipe(writeStream);
+      const writeStream = createWriteStream(destination);
+      fileStream.pipe(writeStream);
 
-    reply.code(201);
-    return { destination };
-  });
+      reply.code(201);
+      return { destination };
+    });
 
-  fastify.delete('/:id', async (req, reply) => {
-    const dir = await readdir(DATA_FOLDER);
+    fastify.delete('/:id', async (req, reply) => {
+      const dir = await readdir(DATA_FOLDER);
 
-    if (!dir.includes(`${req.params.id}.flac`)) {
+      if (!dir.includes(`${req.params.id}.flac`)) {
       return NotFound();
-    }
+      }
 
-    reply.code(204);
-    return rm(join(DATA_FOLDER, `${req.params.id}.flac`));
-  });
+      reply.code(204);
+      return rm(join(DATA_FOLDER, `${req.params.id}.flac`));
+    });
 };
 
 export default soundRoute;
