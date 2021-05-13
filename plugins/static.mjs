201ee3799d9df48f53036c62f494f71a615c6ad3
neolectron <xeolectron@gmail.com>
2021-05-14 00:25:29 +0200

    feat(staticPlugin): log the data folder path on start

--- plugins/static.mjs
+++ plugins/static.mjs
@@ -3,7 +3,7 @@ import staticPlugin from 'fastify-static';
 import { resolve } from 'path';
 
 export default plugin(async function (fastify, opts) {
-  console.log(process.env.DATA_FOLDER);
+  console.log(`using ${process.env.DATA_FOLDER} as data folder`);
   fastify.register(staticPlugin, {
     root: resolve(process.env.DATA_FOLDER),
     prefix: '/data/',
